3e6b86d5f8468ea2da34ec51ab4cf430d2b40c30
SierMoveis <suportesier@gmail.com>
2025-01-10 15:52:52 -0300

    zzz,

--- scripts/back__incial.js
+++ scripts/back__incial.js
@@ -135,7 +135,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/1- Transporte/semana2/1.jpg", "capturas/2024/12-Dezembro/1- Transporte/semana2/2.jpg", "capturas/2024/12-Dezembro/1- Transporte/semana2/3.jpg"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/1- Transporte/semana1/1.jpg", "AplicativoPG/capturas/2025/1- Janeiro/1- Transporte/semana1/2.jpg", "AplicativoPG/capturas/2025/1- Janeiro/1- Transporte/semana1/3.jpg"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/1- Transporte/semana1/1.jpg", "capturas/2025/1- Janeiro/1- Transporte/semana1/2.jpg", "capturas/2025/1- Janeiro/1- Transporte/semana1/3.jpg"] },
         
             // { data: "", captura: ["", "", ""] },
         
@@ -221,7 +221,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/4- Cadastro - Kbm/semana2/pt 01 06-12 à 12-12.png", "capturas/2024/12-Dezembro/4- Cadastro - Kbm/semana2/pt 02 06-12 à 12-12.png", "capturas/2024/12-Dezembro/4- Cadastro - Kbm/semana2/pt 03 06-12 à 12-12.png"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 01 06-01 à 09-01-25.png", "AplicativoPG/capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 02 06-01 à 09-01-25.png", "AplicativoPG/capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 03 06-01 à 09-01-25.png"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 01 06-01 à 09-01-25.png", "capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 02 06-01 à 09-01-25.png", "capturas/2025/1- Janeiro/4- Cadastro - Kbm/semana1/pt 03 06-01 à 09-01-25.png"] },
 
             // { data: "", captura: ["", "", "", ""] },
             
@@ -255,7 +255,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/5- Comercial/semana2/PRINT 01.png", "capturas/2024/12-Dezembro/5- Comercial/semana2/PRINT 02.png", "capturas/2024/12-Dezembro/5- Comercial/semana2/PRINT 03.png"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/5- Comercial/semana2/1.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/5- Comercial/semana2/2.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/5- Comercial/semana2/3.jpeg"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/5- Comercial/semana2/1.jpeg", "capturas/2025/1- Janeiro/5- Comercial/semana2/2.jpeg", "capturas/2025/1- Janeiro/5- Comercial/semana2/3.jpeg"] },
 
             // { data: "", captura: ["", "", "", ""] },
         ],
@@ -287,7 +287,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/6- Compras/semana2/Print 01.png", "capturas/2024/12-Dezembro/6- Compras/semana2/Print 02.png", "capturas/2024/12-Dezembro/6- Compras/semana2/Print 03.png"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/6- Compras/semana1/Print 01.png", "AplicativoPG/capturas/2025/1- Janeiro/6- Compras/semana1/Print 02.png", "AplicativoPG/capturas/2025/1- Janeiro/6- Compras/semana1/Print 03.png"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/6- Compras/semana1/Print 01.png", "capturas/2025/1- Janeiro/6- Compras/semana1/Print 02.png", "capturas/2025/1- Janeiro/6- Compras/semana1/Print 03.png"] },
             
             // { data: "", captura: ["", "", "", ""] },
 
@@ -319,7 +319,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["NÃO FOI ENTREGUE"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/7- Design/semana1/01.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/7- Design/semana1/02.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/7- Design/semana1/03.jpeg"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/7- Design/semana1/01.jpeg", "capturas/2025/1- Janeiro/7- Design/semana1/02.jpeg", "capturas/2025/1- Janeiro/7- Design/semana1/03.jpeg"] },
             
             // { data: "", captura: ["", "", ""] },
         ],
@@ -352,7 +352,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/8- Assistência Comercial/semana2/01 PT.jpeg", "capturas/2024/12-Dezembro/8- Assistência Comercial/semana2/02 PT.jpeg", "capturas/2024/12-Dezembro/8- Assistência Comercial/semana2/G.jpeg"] },
             { data: "18/12/2024", captura: ["NÃO FOI ENTREGUE"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/1.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/2.jpeg", "AplicativoPG/capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/3.jpeg"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/1.jpeg", "capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/2.jpeg", "capturas/2025/1- Janeiro/8- Assistência Comercial/semana2/3.jpeg"] },
 
             // { data: "", captura: ["", "", "", ""] },
 
@@ -603,7 +603,7 @@ const unidades = {
             { data: "12/12/2024", captura: ["capturas/2024/12-Dezembro/17- Geral/Semana 2/geral.jpeg", "images/fraseGeral.png"] },
             { data: "18/12/2024", captura: ["capturas/2024/12-Dezembro/17- Geral/Semana 3/geral.jpeg", "images/fraseGeral.png"] },
 
-            { data: "09/01/2025", captura: ["AplicativoPG/capturas/2025/1- Janeiro/17- Geral/Semana 2/GERAL.jpeg", "images/fraseGeral.png"] },
+            { data: "09/01/2025", captura: ["capturas/2025/1- Janeiro/17- Geral/Semana 2/GERAL.jpeg", "images/fraseGeral.png"] },
 
         ],
             // { data: "", captura: ["", "images/fraseGeral.png"] },
